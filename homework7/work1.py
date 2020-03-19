a3add76cef5cfff9286ebdba87c1921f47852b64
Oleg Chkalov <chkalov66@mail.ru>
2020-03-19 22:49:41 +0500

    update homework7

--- homework7/work1.py
+++ homework7/work1.py
@@ -6,18 +6,28 @@ def cancel_action(func):
 		print('{} is canceled!'.format(func.__name__))
 	return inner
 
-def sum(x, y):
+"""def sum(x, y):
 	return x + y
 
+def mult(x, y):
+    return x * y
 s = cancel_action(sum)
 s_result = s(9, 12)
 print(s_result)
 #s1 = sum(9, 12)
 #print(s1)
 
-def mult(x, y):
-    return x * y
-
 m = cancel_action(mult)
 m(2, 5)
+"""
+@cancel_action
+def sum(x, y):
+	return x + y
 
+print(sum(3, 9))
+
+@cancel_action
+def mult(x, y):
+	return x*y
+
+print(mult(5, 7))
